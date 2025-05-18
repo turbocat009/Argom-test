ec894ec97a05be7a6e2ea55c29355b10983fc916
turbocat009 <76442361+turbocat009@users.noreply.github.com>
2025-05-18 18:43:56 +0200

    Added menu button

--- public/js/openMenu.js
+++ public/js/openMenu.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuButton = document.getElementById("menu-button");
     const menu = document.getElementById("contextMenu");
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
+    const btn3 = document.getElementById("btn3");
+
+
     let menuOpen = false;
     menuButton.addEventListener("click", () => {
         if(menuOpen){
@@ -13,6 +18,10 @@ document.addEventListener("DOMContentLoaded", () => {
         } else{
             menu.classList.remove("none");
             menu.classList.add("divOpen");
+            btn1.classList.add("btn1X");
+            btn2.classList.add("btn2X");
+            btn3.classList.add("btn3X");
+            menuButton.classList.add("moveBtn")
             setTimeout(function () {
                 menu.classList.remove("divOpen");
             }, 499);
